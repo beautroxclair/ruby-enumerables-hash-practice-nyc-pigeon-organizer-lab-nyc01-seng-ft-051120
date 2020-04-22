c6e025cabff138b8c0b7ffa9f26b3fefad4a922f
Beau Troxclair <beau.troxclair@gmail.com>
2020-04-22 05:05:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.reduce({}) do |memo, (key, value)|
+    value.reduce(nil) do |memo_1, (key_1, value_1)|
+      if !memo[value_1]
+        memo[value_1] => {:color => [], :gender => [], :lives => []}
+        memo[value_1][value] = key_1.to_s
+      else 
+        memo[value_1][value] = key_1.to_s
+      end
+    end
+  end
 end
