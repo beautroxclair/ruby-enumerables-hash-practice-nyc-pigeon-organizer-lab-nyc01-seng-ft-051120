f61eb6d03ba8b08c8f38695313204895b79c43c2
Beau Troxclair <beau.troxclair@gmail.com>
2020-04-22 05:35:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,15 @@
 def nyc_pigeon_organizer(data)
   data.reduce({}) do |memo, (key, value)|
-    puts memo 
-    puts key 
-    puts value
-    puts "----"
-    value.reduce({}) do |memo_1, (key_1, value_1)|
-      if !memo[value_1]
-        memo[value_1] = {:color => [], :gender => [], :lives => []}
-        memo[value_1][value] = key_1.to_s
-      else
-        puts memo[value_1]
-        memo[value_1][value] = key_1.to_s
+    value.reduce(nil) do |memo_1, (key_1, value_1)|
+      value_1.map do |el|
+        if !memo[el]
+          memo[el] = {:color =>[], :gender => [], :lives => []}
+          memo[el][key].push(key_1.to_s)
+        else 
+          memo[el][key].push(key_1.to_s)
+        end
       end
     end
+    memo
   end
-  memo
 end
